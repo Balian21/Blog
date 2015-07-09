06e74a1f82c23bae17d3ce89098b3dee48205279
Balian21 <alexpv212121@gmail.com>
2015-07-09 16:15:55 +0300

    Add Ninject

--- src/Blog/Blog/Global.asax.cs
+++ src/Blog/Blog/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Blog.Core;
+using Ninject;
+using Ninject.Web.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,12 +10,28 @@ using System.Web.Routing;
 
 namespace Blog
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : NinjectHttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+
+            kernel.Load(new RepositoryModule());
+            kernel.Bind<IBlogRepository>().To<BlogRepository>();
+
+            return kernel;
+        }
+
+        protected override void OnApplicationStarted()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            base.OnApplicationStarted();
         }
     }
 }
